12f6a352898717f1d9f26d2581bcd8e3694e21c7
Alejandro Toledo <altoledo@pichincha.com>
2023-02-03 12:05:46 -0500

    add feature for delete icon button

--- src/components/molecules/GifCard/GifCard.test.tsx
+++ src/components/molecules/GifCard/GifCard.test.tsx
@@ -6,9 +6,7 @@ describe("GifCard", () => {
   it("should render gif card", () => {
     render(
       <GifCard
-        image={
-          "https://media.tenor.com/Lk1coQ37K0MAAAAS/the-avengers-avengers.gif"
-        }
+        image={"https://media.tenor.com/Lk1coQ37K0MAAAAS/the-avengers-avengers.gif"}
         gifAlt="Imagen gif"
         description={"description"}
       />
@@ -19,28 +17,22 @@ describe("GifCard", () => {
   it("should render the delete icon", () => {
     render(
       <GifCard
-        image={
-          "https://media.tenor.com/Lk1coQ37K0MAAAAS/the-avengers-avengers.gif"
-        }
+        image={"https://media.tenor.com/Lk1coQ37K0MAAAAS/the-avengers-avengers.gif"}
         gifAlt="Imagen gif"
         description={"description"}
-        onClick={onClick}
+        onDelete={onClick}
       />
     );
     const deleteIcon = screen.getByAltText("delete-icon");
     expect(deleteIcon).toBeVisible();
-    fireEvent.click(deleteIcon);
-    expect(onClick).toHaveBeenCalled();
   });
   it("should show delete button on click", async () => {
     render(
       <GifCard
-        image={
-          "https://media.tenor.com/Lk1coQ37K0MAAAAS/the-avengers-avengers.gif"
-        }
+        image={"https://media.tenor.com/Lk1coQ37K0MAAAAS/the-avengers-avengers.gif"}
         gifAlt="Imagen gif"
         description={"description"}
-        onClick={onClick}
+        onDelete={onClick}
       />
     );
     const deleteIcon = screen.getByAltText("delete-icon");
--- src/components/molecules/GifCard/GifCard.tsx
+++ src/components/molecules/GifCard/GifCard.tsx
@@ -1,19 +1,31 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 
 interface GifCardProps {
   image: string;
   gifAlt: string;
   description: string;
-  onClick: React.MouseEventHandler<HTMLDivElement>;
+  onDelete?: React.MouseEventHandler<HTMLElement>;
 }
 
-const GifCard: FC<GifCardProps> = ({ image, gifAlt, onClick }) => {
+const GifCard: FC<GifCardProps> = ({ image, gifAlt, onDelete }) => {
+  const [showDelete, setShowDelete] = useState<boolean>(false);
+  const handleShow = () => {
+    setShowDelete(!showDelete);
+  };
   return (
     <div className="gif-card-container">
       <img src={image} alt={gifAlt} />
-      <div className="delete-icon-container" onClick={onClick}>
+      <div className="delete-icon-container" onClick={handleShow}>
         <img src={"iconurl"} alt={"delete-icon"} />
       </div>
+      {showDelete && (
+        <div>
+          <button type="button" onClick={onDelete}>
+            Eliminar
+          </button>
+          <button type="button">Cancelar</button>
+        </div>
+      )}
     </div>
   );
 };
